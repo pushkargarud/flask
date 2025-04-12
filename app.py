6b699c0ac8a49f96a3a801263a8fbedf5e147543
pushkar garud <pushkrgarud@gmail.com>
2025-04-12 21:40:56 +0100

    Added hobby field and created new tables for NameAge and Hobby

--- app.py
+++ app.py
@@ -4,13 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///names.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://task_manager_myy1_user:DA1Yo4VQBOxB7CXa1Ecs0FULf0K3kUG2@dpg-cvsi74re5dus7394o1bg-a.oregon-postgres.render.com/task_manager_myy1'
 db=SQLAlchemy(app)
 
 class NameAge(db.Model):
+    __tablename__ = 'name_age'
     id=db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     age = db.Column(db.Integer, nullable=False)
+    hobbies = db.relationship('Hobby', backref='person', lazy=True)
+
+
+class Hobby(db.Model):
+    __tablename__='hobby'
+    id=db.Column(db.Integer,primary_key=True)
+    description=db.Column(db.String(200), nullable=False)
+    person_id=db.Column(db.Integer, db.ForeignKey('name_age.id'), nullable=False)
+
+
 
 # In-memory list to store names temporarily
 names_list = []
@@ -25,23 +36,31 @@ def home():
 def result():
     name_input = request.form['name']
     age_int= request.form['age']
+    hobby_input=request.form['hobby']
+
 
     #names_list.append(name_input)  # Store the name in the list
     new_entry =NameAge(name=name_input, age=int(age_int))
     db.session.add(new_entry)
     db.session.commit()
+
+   # Use the new_person.id as FK
+    new_hobby = Hobby(description=hobby_input, person_id=new_entry.id)
+    db.session.add(new_hobby)
+    db.session.commit()
+
     return redirect(url_for('all_names'))
 
 # Display all names and ages
 @app.route('/names')
 def all_names():
-    entries= NameAge.query.all()
-    return render_template('names.html', entries=entries)
+       entries = NameAge.query.all()
+       return render_template('names.html', entries=entries)
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        app.run(debug=True)
+    app.run(debug=True)
 
 @app. route('/age')
 def all_ages():
